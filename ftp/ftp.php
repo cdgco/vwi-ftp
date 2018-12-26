812de1ac6466eaeaedd1f995f0c263a11dfaf038
Carter Roeser <carter@cdgtech.one>
2018-12-26 15:26:17 -0800

    Bug Fixes

--- ftp/ftp.php
+++ ftp/ftp.php
@@ -1,9 +1,29 @@
 <?php
 
 $version = "1.8.8";
-
-require("config.php");
 session_start();
+require( '../../includes/includes.php');
+
+$ftpHost = VESTA_HOST_ADDRESS;
+$ftpPort = "21";
+$ftpMode = "1";
+$ftpSSL  = "0";
+$ftpDir  = "";
+$serverTmp = "";
+$editableExts = "asp,ashx,asmx,aspx,asx,axd,cfm,cgi,css,html,htm,jhtml,js,php,phtml,pl,txt,xhtml,htaccess,log,crt,key,less,scss,md,sql,json";
+$sessionName = "";
+$dateFormatUsa = 1;
+$lockOutTime = 0;
+$versionCheck = 0;
+$showAdvOption = 0;
+$showLockSess = 0;
+$showHostInfo = 0;
+$showAddons = 0;
+$showDotFiles = 1;
+$maxExecTime = 1800;
+$maxFileSize = "1024M";
+
+
 ini_set('max_execution_time', $maxExecTime);
 ini_set('memory_limit', $maxFileSize);
 
--- ftp/index.php
+++ ftp/index.php
@@ -2,22 +2,19 @@
 
 session_start();
 $configlocation = "../../includes/";
-if (file_exists( '../../includes/config.php' )) { require( '../../includes/includes.php'); }  else { header( 'Location: ../../install' );};
+if (file_exists( '../../includes/config.php' )) { require("../../includes/includes.php"); }  else { header( 'Location: ../../install' );};
 
 if(base64_decode($_SESSION['loggedin']) == 'true') {}
 else { header('Location: ../../login.php'); }
 
 $postvars = array(
-    array('hash' => $vst_apikey, 'user' => $vst_username,'password' => $vst_password,'cmd' => 'v-list-user','arg1' => $username,'arg2' => 'json'),
-    array('hash' => $vst_apikey, 'user' => $vst_username,'password' => $vst_password,'cmd' => 'v-list-user-packages','arg1' => 'json')
-);
+    array('hash' => $vst_apikey, 'user' => $vst_username,'password' => $vst_password,'cmd' => 'v-list-user','arg1' => $username,'arg2' => 'json'));
 
 $curl0 = curl_init();
-$curl1 = curl_init();
 $curlstart = 0; 
 
 
-while($curlstart <= 1) {
+while($curlstart <= 0) {
     curl_setopt(${'curl' . $curlstart}, CURLOPT_URL, $vst_url);
     curl_setopt(${'curl' . $curlstart}, CURLOPT_RETURNTRANSFER,true);
     curl_setopt(${'curl' . $curlstart}, CURLOPT_SSL_VERIFYPEER, false);
@@ -28,10 +25,9 @@ while($curlstart <= 1) {
 } 
 
 $admindata = json_decode(curl_exec($curl0), true)[$username];
-$packname = array_keys(json_decode(curl_exec($curl1), true));
 $useremail = $admindata['CONTACT'];
 if(isset($admindata['LANGUAGE'])){ $locale = $ulang[$admindata['LANGUAGE']]; }
-setlocale("LC_CTYPE", $locale); setlocale("LC_MESSAGES", $locale);
+setlocale(LC_CTYPE, $locale); setlocale(LC_MESSAGES, $locale);
 bindtextdomain('messages', '../../locale');
 textdomain('messages');
 
@@ -70,7 +66,8 @@ foreach ($plugins as $result) {
         <link href="../components/animate.css/animate.min.css" rel="stylesheet">
         <link href="../components/animate.css/animate.min.css" rel="stylesheet">
         <link href="../../css/style.css" rel="stylesheet">
-        <link href="../../css/colors/<?php if(isset($_COOKIE['theme'])) { echo base64_decode($_COOKIE['theme']); } else {echo $themecolor; } ?>" id="theme" rel="stylesheet">
+        <link href="../../css/colors/<?php if(isset($_COOKIE['theme']) && $themecolor != 'custom.css') { echo base64_decode($_COOKIE['theme']); } else {echo $themecolor; } ?>" id="theme" rel="stylesheet">
+        <?php if($themecolor == "custom.css") { require( '../../css/colors/custom.php'); } ?>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/6.11.5/sweetalert2.min.css" />
         <?php if(GOOGLE_ANALYTICS_ID != ''){ echo "<script async src='https://www.googletagmanager.com/gtag/js?id=" . GOOGLE_ANALYTICS_ID . "'></script>
         <script>window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', '" . GOOGLE_ANALYTICS_ID . "');</script>"; } ?> 
@@ -182,7 +179,7 @@ foreach ($plugins as $result) {
                     </div>
                 </div>
                 <?php hotkeys($configlocation); ?>
-                <footer class="footer text-center">&copy; <?php echo date("Y") . ' ' . $sitetitle; ?>. <?php echo _("Vesta Web Interface"); ?> <?php require '../../includes/versioncheck.php'; ?> <?php echo _("by CDG Web Services"); ?>.</footer>
+                <footer class="footer text-center">&copy; <?php echo date("Y") . ' ' . $sitetitle; ?>. <?php echo _("Vesta Web Interface"); ?> <?php require '../../includes/versioncheck.php'; ?> <?php echo _("by Carter Roeser"); ?>.</footer>
             </div>
         </div>
         <script src="../components/jquery/jquery.min.js"></script>
